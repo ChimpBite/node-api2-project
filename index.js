a37d62caf166945da2280e47660d1a51fa4baeac
Chad Parker <63312621+ChimpBite@users.noreply.github.com>
2021-03-11 14:43:18 -0700

    wip

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // require your server and launch it here
+require('dotenv').config()
 const server = require('./api/server');
 
-server.listen(5000, () => {
-  console.log('\n*** Server Running on http://localhost:5000 ***\n');
-});
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === 'development') { 
+    const cors = require('cors')
+    server.use(cors())
+}
+
+const PORT = process.env.PORT || 4000
+
+server.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
